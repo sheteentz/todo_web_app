7e0502e46e4fe5d55cc2572025ac69d1dbba35ab
setinc.marko <setinc.marko@gmail.com>
2025-02-06 21:39:20 +0100

    Add new items.

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ from modules import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo_local = st.session_state["new_todo"] + "\n"
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app will increase my productivity")
@@ -13,4 +19,7 @@ for i, todo in enumerate(todos):
 #for todo in todos:
 #    st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo...")
+st.text_input(label="New todo", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
+
+st.session_state
